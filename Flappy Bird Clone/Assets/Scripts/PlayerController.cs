a81cdaec990218ad2808d7ea9e764fcaf8349270
RankeTheDev <RankeTheDev@gmail.com>
2025-07-20 21:34:24 +0200

    Added Jump Mechanic

--- Flappy Bird Clone/Assets/Scripts/PlayerController.cs
+++ Flappy Bird Clone/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     #region VARIABLES
 
-    [SerializeField] private float testfloat = 5f;
-    
+    public Rigidbody2D rb;
+    public bool isDead = false; // Indica si el jugador está muerto
+    [SerializeField] private float jumpForce = 10f; // Fuerza del salto
+
     #endregion
-    
+
     #region METHODS
     // Awake is called once before Start when the script instance is being initialized. Used to set up references.
     void Awake()
     {
-        
+        GetComponent<Rigidbody2D>(); // Obtiene el componente Rigidbody2D del GameObject al que está adjunto este script
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created.
@@ -26,33 +28,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame. Used to see what the player does each frame.
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) // Detecta si se presiona la tecla de espacio o el botón izquierdo del ratón
+        {
+            Jump(); // Llama al método Jump para realizar el salto
+        }
+        
         
     }
-    
-    // FixedUpdate is called at fixed intervals, independently of the frame rate. Used for physics related calculations.
-    void FixedUpdate()
+
+    void Jump()
     {
-        
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // Aplica una fuerza hacia arriba al Rigidbody2D
+        Debug.Log("Salto realizado");
     }
-    
-    // LateUpdate is called once per frame, but after all Update functions have completed.
-    void LateUpdate()
-    {
-        
-    }
-    
-    IEnumerator MiCorrutina()
-    {
-        Debug.Log("Inicio de la corrutina");
-        
-        yield return new WaitForSeconds(2f); // Espera 2 segundos
-        
-        Debug.Log("Después de la espera");
-    
-        yield return null; // Espera hasta el siguiente frame
-    
-        Debug.Log("Final de la corrutina");
-    }
+
     #endregion
 
 }
